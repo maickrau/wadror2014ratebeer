2286097ca8f1f5f688d8f2a46214e32a45a14c9e
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-01-28 12:49:00 +0200

    viikko 3 tehtävä 6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,8 +5,12 @@ class Brewery < ActiveRecord::Base
   has_many :ratings, :through => :beers
 
   validates :name, :presence => true
-  validates :year, :numericality => { less_than_or_equal_to: 2014,
-                                      greater_than_or_equal_to: 1042,
+  validates :year, :numericality => { greater_than_or_equal_to: 1042,
                                       only_integer: true}
+  validate :year_is_not_in_the_future
+
+  def year_is_not_in_the_future
+    errors.add(:year, "must not be in the future") if year > Time.new.year
+  end
 
 end
