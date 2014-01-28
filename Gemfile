ed897397878ee2232ce6b77802c36c05570cc683
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-01-28 12:17:31 +0200

    viikko 3 tehtävä 10

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gem 'rb-readline'
 
 gem 'byebug', group: [:development, :test]
 
+gem 'bcrypt-ruby', '~> 3.1.2'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ActiveRecord::Base
   include RatingAverage
 
+  has_secure_password
+
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 15 }
+  validates :password, length: { minimum: 4 }
+  validate :password_is_not_completely_stupid, on: :create
 
   has_many :ratings
+  has_many :beers, :through => :ratings
+
+  def password_is_not_completely_stupid
+    errors.add(:password, 'must contain an upper case letter') if password == password.downcase
+    errors.add(:password, 'must contain a lower case letter')  if password == password.upcase
+    errors.add(:password, 'must contain a number') if not password =~ /\d/
+  end
 
 end
