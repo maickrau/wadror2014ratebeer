ef1a8914a36a2e6f6d87f2ec4642b3830fa6ba48
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-01-28 11:11:12 +0200

    viikko 3 tehtävä 2

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Ratebeer::Application.routes.draw do
 
   resources :sessions, :only => [:new, :create]
 
+  get 'signin', to: 'sessions#new'
+
+  delete 'signout', to: 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
