644ccf31450491c508382eb1a61063d9c6786c50
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-01-28 11:52:24 +0200

    viikko 3 tehtävä 5

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -4,4 +4,9 @@ class Brewery < ActiveRecord::Base
   has_many :beers, :dependent => :destroy
   has_many :ratings, :through => :beers
 
+  validates :name, :presence => true
+  validates :year, :numericality => { less_than_or_equal_to: 2014,
+                                      greater_than_or_equal_to: 1042,
+                                      only_integer: true}
+
 end
