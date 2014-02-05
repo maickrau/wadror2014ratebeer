471afe06a91e2ac561ba4625502d999df8bf72a5
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-02-05 13:18:49 +0200

    viikko 4 tehtävä 10

---
+++ spec/models/dummy_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+#mainitsee beerclubin ja membershipin jotta simplecov tajuaa niiden olevan olemassa
+
+describe BeerClub do
+end
+
+describe BeerClubsController do
+end
+
+describe MembershipsController do
+end
+
+describe Membership do
+end  
