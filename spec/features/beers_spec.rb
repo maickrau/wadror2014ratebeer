2bf43e8bc199b673cb9384646f14c624853e48d7
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-02-06 11:10:31 +0200

    viikko 4 tehtävä 14

--- spec/features/beers_spec.rb
+++ spec/features/beers_spec.rb
@@ -5,20 +5,25 @@ describe 'Beer' do
     FactoryGirl.create(:brewery)
   end
 
-  it 'with a proper name can be saved through the beers view' do
-    visit beers_path
-    click_link 'New Beer'
-    fill_in('beer_name', with:'kalia')
-    click_button 'Create Beer'
-    expect(current_path).to eq(beers_path)
-    expect(page).to have_content 'kalia'
-  end
+  describe "when user is logged in" do
+    before :each do
+      FactoryGirl.create(:user)
+      sign_in(username:'Pekka', password:'Foobar1')
+    end
 
-  it "without a proper name won't be saved through the beers view" do
-    visit beers_path
-    click_link 'New Beer'
-    click_button 'Create Beer'
-    expect(page).to have_content "Name can't be blank"
-    expect(Beer.count).to eq(0)
+    it 'beer with a proper name can be saved through the beers view' do
+      visit new_beer_path
+      fill_in('beer_name', with:'kalia')
+      click_button 'Create Beer'
+      expect(current_path).to eq(beers_path)
+      expect(page).to have_content 'kalia'
+    end
+
+    it "beer without a proper name won't be saved through the beers view" do
+      visit new_beer_path
+      click_button 'Create Beer'
+      expect(page).to have_content "Name can't be blank"
+      expect(Beer.count).to eq(0)
+    end
   end
 end
